dce15722046987ddaf6c06aed0817ec4bffddce9
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-16 00:58:58 +0530

    Auto commit

---
+++ pattern9.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        for (int j = 1; j <= N - i; j++){
+            cout << " ";
+        }
+        for (int j = 1; j <= i * 2 - 1; j++){
+            cout << "*";
+        }
+        for (int j = 1; j <= N - i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= N; i++){
+        for (int j = 1; j <= i - 1; j++){
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * (N - i) + 1; j++){
+            cout << "*";
+        }
+        for (int j = 1; j <= i - 1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}

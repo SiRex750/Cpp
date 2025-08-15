bb38290daf64ff308dadb563b6862c62cd8b8cc9
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-16 02:14:59 +0530

    Auto commit

---
+++ pattern11.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i; j++){
+            if((i+j) % 2 == 0){
+                cout << 1;
+            }
+            else {
+                cout << 0;
+            }
+        }
+        cout << endl;
+    }
+}

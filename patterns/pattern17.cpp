784e7b23c5220718091d6c78fda6a1b036628ec2
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-16 11:23:21 +0530

    Auto commit

---
+++ patterns/pattern17.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j++){
+            cout << (char)(j + 65);
+        }
+        for(int j = (i - 1); j >= 0; j--){
+            cout << char(j + 65);
+        }
+        cout << endl;
+    }
+}

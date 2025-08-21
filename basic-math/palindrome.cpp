7ccefb20b34bb997e2480329627a404f77ad68b6
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-21 21:30:38 +0530

    Auto commit

--- basic-math/palindrome.cpp
+++ basic-math/palindrome.cpp
@@ -5,11 +5,13 @@ int main() {
     int N;
     cin >> N;
     int revNum = 0;
+    int dup = N;
     while(N != 0){
         int ld = N % 10;
         revNum = (revNum * 10) + ld;
         N = N/10;
     }
-    cout << revNum;
+    if (dup == revNum) cout << "true" << endl;
+    else cout << "false" << endl;
     return 0;
 }

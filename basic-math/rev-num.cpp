f3ce94dcc2d7c6081c1f67aee827abfb71656e33
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-21 21:12:30 +0530

    Auto commit

---
+++ basic-math/rev-num.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int revNum = 0;
+    while(N > 0){
+        int ld = N % 10;
+        revNum = (revNum * 10) + ld;
+        N = N/10;
+    }
+    return 0;
+}

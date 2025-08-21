7db870120ac4967da9b042dfde7f7bceeac88376
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-21 20:51:27 +0530

    Auto commit

--- basic-math/count-digits.cpp
+++ basic-math/count-digits.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
     int N, digits;
     cin >> N;
+    /*
+    while(N > 0){
+        N = N/10;
+        digits++;
+    }
+    */
     digits = (int)(log10(N) + 1);
     cout << digits;
     return 0;

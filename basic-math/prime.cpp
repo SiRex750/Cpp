00c88f108d31457f8b6a50e35bbdf91e324c5ac3
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-23 02:06:09 +0530

    Auto commit

---
+++ basic-math/prime.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int count = 0;
+    for(int i = 1; i* i <= N; i++){
+        if(N % i == 0){
+            count++;
+            if((N / i) != i) count++;
+        }
+    }
+    if(count == 2) cout << "true";
+    else cout << "false";
+    return 0;
+}

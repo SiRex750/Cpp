b75a335930b42d4e1bc211183877e5a13de4209a
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-23 15:03:28 +0530

    Auto commit

---
+++ recursion/1-to-n.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void f(int i, int n){
+    if(i > n){
+        return;
+    }
+    cout << i << " ";
+    f(i + 1, n);
+}
+
+int main() {
+    int n, i = 1;
+    cin >> n;
+    f(i, n);
+    return 0;
+}

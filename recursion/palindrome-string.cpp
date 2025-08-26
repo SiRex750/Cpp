41ff4c461a9585aa0ac546e668b5d27ea03d9d8e
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-26 20:56:43 +0530

    Auto commit

--- recursion/palindrome-string.cpp
+++ recursion/palindrome-string.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isPalinfrome(string &s, int i){
+bool isPalindrome(string &s, int i){
     if(i >= s.size()/2){
         return true;
     }
     if(s[i] != s[s.size() - 1 - i]){
         return false;
     }
-    return isPalinfrome(s, i + 1);
+    return isPalindrome(s, i + 1);
 }
 
 int main() {
     string s;
     cin >> s; // getline(cin, s); for sentence with spaces
-    if(isPalinfrome(s, 0)){
+    if(isPalindrome(s, 0)){
         cout << "YES";
     } else {
         cout << "NO";

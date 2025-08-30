3017aa9f9c9816c941c0435aaaf846e5b971d523
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-30 12:59:22 +0530

    Auto commit

---
+++ hashing/num-hashing.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int hashh[100000000];
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // precompute
+    for(int i = 0; i < n; i++) {
+        hashh[arr[i]] += 1;
+    }
+    int q;
+    cin >> q;
+    while(q--) {
+        int number;
+        cin >> number;
+        // fetch
+        cout << hashh[number] << endl;
+    }
+    return 0;
+}

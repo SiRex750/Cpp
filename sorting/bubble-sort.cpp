b96c92385d9cd2c16d61028792eea44e7b36f5aa
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-31 23:28:35 +0530

    Auto commit

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) {
     for (int i = n - 1; i >= 0; i--) {
+        //optimization counter
+        int didSwap = 0;
         for (int j = 0; j <= i - 1; j++) {
             if (arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
+                //optimization counter increment
+                didSwap = 1;
         }
+        //optimization - early break if pre-swapped
+        if(didSwap == 0)
+            break;
     }
 }
 

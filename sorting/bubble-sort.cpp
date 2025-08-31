1e30a1a0609586b49318372f371b6f5d41397039
Siddanth Anil <119781835+SiRex750@users.noreply.github.com>
2025-08-31 23:21:50 +0530

    Auto commit

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -1,14 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    for(int i = 0; i < n-1; i++){
-        int min = i;
-        for(int j = i+1; j < n; j++){
-            if(arr[j] < arr[min])
-                min = j;
+void bubbleSort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
         }
-        swap(arr[i], arr[min]);
     }
 }
 
@@ -18,7 +16,7 @@ int main() {
     int arr[n];
     for(int i = 0; i < n; i++)
         cin >> arr[i];
-    selectionSort(arr, n);
+    bubbleSort(arr, n);
     for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
